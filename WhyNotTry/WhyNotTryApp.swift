7571d6bb2d8f490acafc96eb7e26d4f0ccc4df2c
Jannik Scheider <js509@hdm-stuttgart.de>
2024-09-14 16:40:13 +0200

    first commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Jannik Scheider on 08.04.24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
